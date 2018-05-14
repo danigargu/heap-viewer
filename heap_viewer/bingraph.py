88ce2dcb911047f8f62f221378d15cee48d76403
Daniel García <danigargu@gmail.com>
2018-05-14 13:21:26 +0200

    refactor bingraph

--- heap_viewer/bingraph.py
+++ heap_viewer/bingraph.py
@@ -4,12 +4,11 @@
 # HeapViewer - by @danigargu
 #
 
+import idc
 import idaapi
 from misc import *
 
 # --------------------------------------------------------------------------
-# TODO: refactor this
-
 class BinGraph(idaapi.GraphViewer):
     def __init__(self, heap, info, close_open=True):   
         self.heap       = heap
@@ -44,21 +43,42 @@ class BinGraph(idaapi.GraphViewer):
     def warning_line(self, txt):
         return idaapi.COLSTR(txt, idaapi.SCOLOR_ERROR)
 
-
     def chunk_info(self, chunk_addr, chunk):
-        line1 = idaapi.COLSTR("Chunk ", idaapi.SCOLOR_NUMBER)
-        line2 = idaapi.COLSTR("0x%x\n\n" % (chunk_addr), idaapi.SCOLOR_INSN)
-        line3 = idaapi.COLSTR("size: 0x%x\nfd: 0x%x - %s" % 
+        line =  idaapi.COLSTR("Chunk ", idaapi.SCOLOR_NUMBER)
+        line += idaapi.COLSTR("0x%x\n\n" % (chunk_addr), idaapi.SCOLOR_INSN)
+        line += idaapi.COLSTR("size: 0x%x\nfd: 0x%x - %s" % 
                 (chunk.size, chunk.fd, SegName(chunk.fd), ), SCOLOR_DEFAULT)
-        return line1 + line2 + line3
-
+        return line
 
     def tcache_info(self, entry_addr, chunk_addr):        
-        line1 = idaapi.COLSTR("entry: ", idaapi.SCOLOR_NUMBER)
-        line2 = idaapi.COLSTR("0x%x\n" % (entry_addr), idaapi.SCOLOR_INSN)
-        line3 = idaapi.COLSTR("chunk: ", idaapi.SCOLOR_NUMBER)
-        line4 = idaapi.COLSTR("0x%x" % (chunk_addr), idaapi.SCOLOR_INSN)
-        return line1 + line2 + line3 + line4
+        line =  idaapi.COLSTR("entry: ", idaapi.SCOLOR_NUMBER)
+        line += idaapi.COLSTR("0x%x\n" % (entry_addr), idaapi.SCOLOR_INSN)
+        line += idaapi.COLSTR("chunk: ", idaapi.SCOLOR_NUMBER)
+        line += idaapi.COLSTR("0x%x" % (chunk_addr), idaapi.SCOLOR_INSN)
+        return line
+
+    def bin_info(self, node_title, chunk_addr, chunk, with_size=True):
+        line =  idaapi.COLSTR("%s " % node_title, idaapi.SCOLOR_NUMBER)
+        line += idaapi.COLSTR("0x%x\n\n" % chunk_addr, idaapi.SCOLOR_INSN)
+
+        chunk_info = ""
+        if with_size:
+            chunk_info += "size: 0x%x\n" % chunk.size
+
+        chunk_info += "fd: 0x%x - %s\nbk: 0x%x - %s" % (chunk.fd, SegName(chunk.fd), 
+            chunk.bk, SegName(chunk.bk))
+
+        line += idaapi.COLSTR(chunk_info, SCOLOR_DEFAULT)
+        return line
+
+    def add_error_edge(self, id_node):
+        warn = self.warning_line("[...] - List corrupted or infinite cycle detected")
+        id_end = self.AddNode( (True, "[...]", warn, None) )
+        self.AddEdge(id_node, id_end)  
+
+    def add_tail_edge(self, id_node):
+        id_end = self.AddNode( (True, "TAIL - 0", "TAIL", None) )
+        self.AddEdge(id_node, id_end)
 
 
     def make_fastbin_graph(self):
@@ -71,8 +91,7 @@ class BinGraph(idaapi.GraphViewer):
             warning("Empty fastbin entry")
             return False
 
-        line1 = idaapi.COLSTR("FASTBIN - 0x%02X" % size, idaapi.SCOLOR_ERROR)
-        id_header = self.AddNode( (True, "fastbin[%x]" % size, "FASTBIN - 0x%02X" % size) )  
+        id_header = self.AddNode( (True, "fastbin[%x]" % size, "FASTBIN - 0x%02X" % size, None) )  
         id_chunk = id_header
 
         chain, c_error = self.heap.chunk_chain(fastbin, stop=0, add_stop=False)
@@ -81,16 +100,13 @@ class BinGraph(idaapi.GraphViewer):
             chunk_info = self.heap.get_chunk(chunk_addr)
 
             prev_chunk = id_chunk
-            id_chunk = self.AddNode( (True, str(chunk_info), self.chunk_info(chunk_addr, chunk_info)) )
+            id_chunk = self.AddNode( (True, str(chunk_info), self.chunk_info(chunk_addr, chunk_info), chunk_addr) )
             self.AddEdge(prev_chunk, id_chunk)
 
         if c_error: 
-            warn = self.warning_line("[...] - List corrupted or infinite cycle detected")
-            id_end = self.AddNode( (True, "[...]", warn) )
-            self.AddEdge(id_chunk, id_end)            
+            self.add_error_edge(id_chunk)          
         else: 
-            id_end = self.AddNode( (True, "TAIL - 0", "TAIL") )
-            self.AddEdge(id_chunk, id_end)
+            self.add_tail_edge(id_chunk)
 
         return True
 
@@ -106,7 +122,7 @@ class BinGraph(idaapi.GraphViewer):
             return False
 
         line_header = "TCACHE[%d] - 0x%02X\nCounts: %d\n" % (entry_id, entry_size, tcache_entry['counts'])
-        id_header = self.AddNode( (True, line_header, line_header) )
+        id_header = self.AddNode( (True, line_header, line_header, None) )
         id_chunk = id_header
 
         chain, c_error = self.heap.tcache_chain(tcache_entry['next'], add_stop=False)
@@ -116,58 +132,46 @@ class BinGraph(idaapi.GraphViewer):
             chunk_info = self.heap.get_chunk(chunk_addr)
 
             prev_chunk = id_chunk
-            id_chunk = self.AddNode( (True, str(chunk_info), self.tcache_info(mem_addr, chunk_addr)) )
+            tcache_info = self.tcache_info(mem_addr, chunk_addr)
+            id_chunk = self.AddNode( (True, str(chunk_info), tcache_info, chunk_addr) )
             self.AddEdge(prev_chunk, id_chunk)
 
         if c_error: 
-            warn = self.warning_line("[...] - List corrupted or infinite cycle detected")
-            id_end = self.AddNode( (True, "[...]", warn) )
-            self.AddEdge(id_chunk, id_end)
-            
+            self.add_error_edge(id_chunk)           
         else: 
-            id_end = self.AddNode( (True, "TAIL - 0", "TAIL") )
-            self.AddEdge(id_chunk, id_end)
+            self.add_tail_edge(id_chunk)
 
         return True
 
-    
+
     def make_bin_graph(self):
-        id_node = dict()
+        id_node  = dict()
         bin_base = self.info['bin_base']
-        chunk = self.heap.get_chunk(bin_base)
 
-        line1 = idaapi.COLSTR("%s " % self.bin_type,idaapi.SCOLOR_NUMBER)
-        line2 = idaapi.COLSTR("0x%x\n\n" % bin_base, idaapi.SCOLOR_INSN)
-        line3 = idaapi.COLSTR("size: 0x%x\nfd: 0x%x - %s\nbk: 0x%x - %s" % 
-                (chunk.size, chunk.fd, SegName(chunk.fd), chunk.bk, SegName(chunk.bk)), SCOLOR_DEFAULT)
+        id_chunk = None
+        prev_chunk = None
+        chain, c_error = self.heap.chunk_chain(bin_base, stop=bin_base, add_stop=False)
+        
+        for i, chunk_addr in enumerate(chain):
+            header = self.bin_type if i == 0 else 'Chunk'
+            chunk_info = self.heap.get_chunk(chunk_addr)
 
-        line = line1+line2+line3
+            node_info = self.bin_info(header, chunk_addr, chunk_info, i!=0)
+            id_chunk = self.AddNode( (True, str(chunk_info), node_info, chunk_addr) )
+            id_node[chunk_addr] = id_chunk
 
-        id_base = self.AddNode( (True, str(chunk), line )) 
-        id_node[bin_base] = id_base
+            if prev_chunk is not None:
+                self.AddEdge(prev_chunk, id_chunk)
 
-        chunks  = []
-        temp_fd = chunk.fd
-        base = id_base
+            prev_chunk = id_chunk
 
-        while temp_fd != bin_base:
+        if c_error: 
+            self.add_error_edge(prev_chunk)
+        else:
+            self.AddEdge(prev_chunk, id_node[bin_base])
 
-            chunk = self.heap.get_chunk(temp_fd)
-
-            line1 = idaapi.COLSTR("Chunk ", idaapi.SCOLOR_NUMBER)
-            line2 = idaapi.COLSTR("0x%x\n\n" % (temp_fd), idaapi.SCOLOR_INSN)
-            line3 = idaapi.COLSTR("size: 0x%x\nfd: 0x%x - %s\nbk: 0x%x - %s" % 
-                (chunk.size, chunk.fd, SegName(chunk.fd), chunk.bk, SegName(chunk.bk)), SCOLOR_DEFAULT)
-
-            id_chunk = self.AddNode( (True, str(chunk), line1 + line2 + line3) )
-            
-            self.AddEdge(id_base, id_chunk)
-
-            temp_fd = chunk.fd
-            id_base = id_chunk
-
-        self.AddEdge(id_base, id_node[bin_base])
         return True
+
 
     def OnRefresh(self):
         self.Clear()
@@ -177,14 +181,16 @@ class BinGraph(idaapi.GraphViewer):
         return self[node_id][1]
 
     def OnGetText(self, node_id):
-        is_thread, value, label = self[node_id]
-        if is_thread:
-            #return (label, 0xf5f5f5)
+        default_color = idc.DEFCOLOR
+        flag, value, label, address = self[node_id]
+        if flag:
             return (label, 0xf5f5f5)
         return label
 
     def OnDblClick(self, node_id):
-        # TODO
+        address = self[node_id][3]
+        if address is not None:
+            idc.Jump(address)
         return True
 
     def OnCommand(self, cmd_id):
